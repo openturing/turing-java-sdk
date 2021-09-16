328536041b4d7e9350b5e2b648d182d42aa372d3
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2021-09-16 17:14:12 -0300

    - Facet

---
+++ src/main/java/com/viglet/turing/client/sn/facet/TurSNFacetFieldValue.java
@@ -0,0 +1,34 @@
+package com.viglet.turing.client.sn.facet;
+
+public class TurSNFacetFieldValue {
+	
+	private String label;
+	
+	private String apiURL;
+	
+	private int count;
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getApiURL() {
+		return apiURL;
+	}
+
+	public void setApiURL(String apiURL) {
+		this.apiURL = apiURL;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+}
